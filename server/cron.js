eb7f5805ab98ebbe50c7acab5a67fed78c0a50cc
Omar Gutierrez Condori <omargc.mus@gmail.com>
2020-10-20 13:36:34 -0400

    cambiando a cronjob

--- server/cron.js
+++ server/cron.js
@@ -1,14 +1,12 @@
+const rp = require('request-promise')
 const redis = require('redis')
-const fastify = require('fastify')({ logger: true })
-const port = process.env.PORT || 3400
+const time = 3600 // Tiempo en segundos que dura el cacheo en Redis
 
 const client = redis.createClient()
 
 client.on('error', (err) => {
   console.log('Error REDIS: ' + err)
 })
-
-fastify.register(require('fastify-cors'), {})
 
 const sitios = {
   bolivia: {
@@ -57,48 +55,29 @@ const sitios = {
   }
 }
 
-fastify.get('/computo/:sitio', async (req, reply) => {
-  const { sitio } = req.params
-
-  if (!sitio) {
-    return { error: 'Debe enviar el sitio a buscar' }
-  }
-
-  if (!sitios[sitio]) {
-    return { error: `No existe el sitio: ${sitio}` }
-  }
-
-  try {
-    const result = await getQuery(sitios[sitio].url, sitio, sitios[sitio].body)
-    return result
-  } catch (e) {
-    return { error: e.message }
-  }
-})
-
 function getQuery (url = '', path = '', body = {}) {
   return new Promise((resolve, reject) => {
-    client.get(`computo:${path}`, (err, result) => {
-      if (err) {
-        return reject(err)
-      }
-      if (result) {
-        resolve(JSON.parse(result))
-      } else {
-        reject(new Error('No existe el registro'))
-      }
-    })
+    const searchUrl = `https://computo.oep.org.bo/api/v1/resultado/${url}`
+
+    rp({ method: 'POST', uri: searchUrl, body, json: true })
+      .then(response => {
+        client.setex(`computo:${path}`, time, JSON.stringify({ source: 'Redis Cache', ...response }))
+        resolve({ source: 'Computo API', ...response })
+      })
+      .catch(err => {
+        reject(err)
+      })
   })
 }
 
-// Run the server!
-const start = async () => {
+(async () => {
   try {
-    await fastify.listen(port)
-    fastify.log.info(`server listening on ${fastify.server.address().port}`)
-  } catch (err) {
-    fastify.log.error(err)
-    process.exit(1)
+    for (let sitio in sitios) {
+      const result = await getQuery(sitios[sitio].url, sitio, sitios[sitio].body)
+      console.log(sitio.toUpperCase() + ':', result.correcto)
+    }
+  } catch (error) {
+    console.log('ERROR CRON:', error.message)
   }
-}
-start()
+  process.exit(1)
+})()
